ba9ff9cfa20bfdcc22abbc55bf9fa96c01d7aa61
Adriele Lima <adriele.p.t@hotmail.com>
2023-08-14 20:10:24 +0000

    Adding basic templates for editing

--- my_recipes/models.py
+++ my_recipes/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+
+class Post(models.Model):
+    featured_image = CloudinaryField('image', default='placeholder')
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='recipes')
+    created_on = models.DateTimeField(auto_now=True)
+    ingredients_content = models.TextField()
+    method_content = models.TextField()
+    likes = models.ManyToManyField(User, related_name='recipe_likes', blank=True)
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return self.title
+
+    def number_of_likes(self):
+        return self.likes.count()
--- my_recipes/views.py
+++ my_recipes/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Post
 
-# Create your views here.
+
+class PostList(generic.ListView):
+    model = Post
+    template_name = 'index.html'
