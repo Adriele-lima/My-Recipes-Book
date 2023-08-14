2e3451c72add956e3642f7913212c8b54369bbfe
Adriele Lima <adriele.p.t@hotmail.com>
2023-08-14 23:23:20 +0000

    Adding summernote and changing size of pictures

--- my_recipe_book/urls.py
+++ my_recipe_book/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('summernote/', include('django_summernote.urls')),
     path('', include('my_recipes.urls'), name='my_recipes_urls'),
 ]
--- my_recipes/admin.py
+++ my_recipes/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
 from .models import Post
+from django_summernote.admin import SummernoteModelAdmin
 
-admin.site.register(Post)
+
+@admin.register(Post)
+class PostAdmin(SummernoteModelAdmin):
+
+    prepopulated_fields = {'slug': ('title',)}
+    list_display = ('title', 'slug', 'created_on')
+    search_fields = ['title']
+    summernote_fields = ('ingredients_content', 'method_content')
