7b9a26ce0d5e32de9b33775f2864fed054b8d9d1
Adriele Lima <adriele.p.t@hotmail.com>
2023-08-25 13:57:52 +0000

    Adding files and registration function

--- my_recipe_book/urls.py
+++ my_recipe_book/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
     path('', include('my_recipes.urls'), name='my_recipes_urls'),
+    path('accounts/', include('allauth.urls')),
 ]
